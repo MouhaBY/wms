c58377f12457f2c909fee0cba5c91e54a78de798
Mohammed BY <mohammed.beny@hotmail.fr>
2022-03-15 13:13:59 +0100

    drawer

--- src/Components/Main/Drawer.js
+++ src/Components/Main/Drawer.js
@@ -1,47 +1,47 @@
 import React from "react";
-import { NavLink as RouterLink } from "react-router-dom";
-import { MENUS } from "./UrlHandler";
-import { useNavigate } from "react-router-dom";
-import { checkAccess } from "../../features/access";
 import { useSelector } from "react-redux";
-import { selectProfile } from "../../utils/selectors";
+import { NavLink as RouterLink, useNavigate } from "react-router-dom";
+import { MENUS } from "./UrlHandler";
+import { checkAccess } from "../../features/access";
+import { selectProfile, selectShowDrawer } from "../../utils/selectors";
 import WarehouseRoundedIcon from "@mui/icons-material/WarehouseRounded";
 
 
 export default function Drawer(){
     const navigate = useNavigate();
     const profile = useSelector(selectProfile());
+    const showDrawer = useSelector(selectShowDrawer());
 
     return(
-        <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style={{width: 280}}>
+        <div className={"d-flex flex-column flex-shrink-0 bg-dark " + (showDrawer ? "p-3 text-white" : "")} style={{width: showDrawer ? 280 : 60}}>
             <div 
-                className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none" 
+                className={"d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"} 
                 onClick={()=>navigate("/")}
             >
-                <div className="bi me-2" style={{width:40, height:32}}>
+                <svg className={"bi "+ (showDrawer ? "me-2" : "")} style={{width:40, height:32}}>
                     <WarehouseRoundedIcon fontSize="large" color="primary"/>
-                </div>
-                <h2 className="fs-4">WMS</h2>
+                </svg>
+                {
+                    showDrawer && <h2 className="fs-4">WMS</h2>
+                }
             </div>
             <hr />
-            <ul className="nav nav-pills flex-column mb-auto">
+            <ul className="nav nav-pills flex-column mb-auto text-center">
                 {
                     MENUS.map((menu, index)=>(
-                        checkAccess(profile, menu.route) ? 
+                        checkAccess(profile, menu.route) && 
                             <li key={index} className="nav-item">
                                 <RouterLink 
                                     to={menu.route} 
                                     className={"d-flex flex-row align-items-center nav-link " + (RouterLink.activeClassName ? "active" : "text-white")} 
                                     style={{ textDecoration: "none", color: "inherit" }}
                                 >
-                                    <svg className="bi me-2" style={{width:20, height:20}} >
+                                    <svg className="bi me-2" style={{width:showDrawer ? 20 : 24, height:showDrawer ? 20 : 24}} >
                                         {menu.icon}
-                                        
                                     </svg>
-                                    {menu.name}
+                                    {showDrawer && menu.name}
                                 </RouterLink> 
                             </li>
-                            : null
                     ))
                 }
             </ul>
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,19 +2,14 @@ import React from "react";
 import AppBar from "./AppBar";
 import Drawer from "./Drawer";
 import UrlHandler from "./UrlHandler";
-import { selectShowDrawer } from "../../utils/selectors";
-import { useSelector } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 
-export default function Main (){
-    const showDrawer = useSelector(selectShowDrawer());
 
+export default function Main (){
     return(
         <div className="main-div">
             <Router>
-                { showDrawer &&
-                    <Drawer />
-                }
+                <Drawer />
                 <div className="main-div-div">
                     <AppBar />
                     <div className="main-div-url">
